4f25a2c9d70dbf01e83a8ea334ca54d7039fb5d0
hugofluhr <hugo.fluhr@gmail.com>
2023-01-09 21:07:02 +0100

    brain Class

---
+++ gsp_neuro/brain.py
@@ -0,0 +1,29 @@
+from pygsp import graphs
+from gsp_neuro import data_loading as dload
+
+class Brain:
+    def __init__(self, data_path, sub_ID, scale, field = 'fibDensity') -> None:
+        self.data_path = data_path
+        self.scale = scale
+        self.subject_dir = dload.get_subject_dir(data_path, sub_ID)
+        self.sub_ID = sub_ID
+        self.path = dload.get_sub_connectomes_paths(self.subject_dir, self.scale)[0]
+        self.field = field
+        self.signals = {}
+
+    def load_graph(self, lap_type = 'normalized'):
+        self.G = graphs.Graph(dload.load_connectome(self.path, field=self.field), lap_type=lap_type)
+    
+        # node coordinates :
+        df_coords = dload.read_coords(scale=self.scale)
+        self.G.set_coordinates(kind=df_coords[['XCoord(mm)','YCoord(mm)','ZCoord(mm)']].to_numpy())
+        self.rois = list(df_coords[df_coords["Structures Names"].str.contains('ctx')]["Structures Names"].str.replace('ctx-',''))
+        rois_ids = list(df_coords[df_coords["Structures Names"].str.contains('ctx')].index)
+        self.G = self.G.subgraph(rois_ids)
+        self.adjacency = self.G.W.toarray()
+        self.G.compute_fourier_basis()
+
+    def add_signal(self, signal_type):
+        self.signals.update({signal_type : dload.get_signal(self.data_path, self.sub_ID, 
+                                                signal_type, self.scale)})
+
